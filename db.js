b3b183f5298e94225c106733a4c945fa838f7065
kozak201201 <kozak20012001@gmail.com>
2021-09-22 13:10:34 +0300

    change db

--- db.js
+++ db.js
@@ -4,9 +4,16 @@ module.exports = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
     process.env.DB_PASSWORD,{
-        dialect: 'postgres', 
+        dialect: 'postgres',
+        ssl: true,
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false
+            }
+        },
         host: process.env.DB_HOST, 
-        port: process.env.DB_PORT, 
+        port: process.env.DB_PORT,
         define: {
             timestamps: false
         }
