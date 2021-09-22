880c126ad4de9d43879c5f1c73a7e84d003dbbbe
kozak201201 <kozak20012001@gmail.com>
2021-09-22 12:59:35 +0300

    changes

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ module.exports = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
     process.env.DB_PASSWORD,{
-        dialect: 'mysql', 
+        dialect: 'postgres', 
         host: process.env.DB_HOST, 
         port: process.env.DB_PORT, 
         define: {
